ee846194d9adf961e08d031f6505917ca76a2e2f
Ahmad Amine <ahmadamine998@gmail.com>
2023-08-16 00:44:27 +0900

    Fixed dimension bug in initial state in simulate

--- system/system.py
+++ system/system.py
@@ -83,10 +83,10 @@ class SystemLearning:
         :return:
         """
 
-        if input_array.dim() == 3:
-            batch_size, time_length, num_inputs = input_array.shape
-        elif input_array.dim() == 2:
-            time_length, num_inputs = input_array.shape
+        if true_state.dim() == 3:
+            batch_size, time_length, _ = input_array.shape
+        elif true_state.dim() == 2:
+            time_length, _ = input_array.shape
             batch_size = 1
         else:
             # raise
@@ -95,7 +95,7 @@ class SystemLearning:
         if initial_state is not None:
             state = initial_state
         else:
-            state = torch.zeros((batch_size, num_inputs))
+            state = torch.zeros((batch_size, self.num_states))
         loss = 0
         state_array = torch.zeros((batch_size, time_length, self.num_states))  # BATCH x TIME x STATES
         for t in range(time_length):
@@ -156,86 +156,6 @@ class SystemLearning:
         ax.minorticks_on()
         plt.legend()
         plt.show()
-
-    # def get_data_from_dict(self, data_type='input', data_use='training'):
-    #     """
-    #     This function converts data from dictionary and creates a list of torch tensors for easier training.
-    #     :param data_type:
-    #     :param data_use:
-    #     :return:
-    #     """
-    #     data_out = []
-    #     data_source = None
-    #     if data_use == "training":
-    #         data_source = self.training_data
-    #     if data_use == "testing":
-    #         data_source = self.testing_data
-    #     if data_type == 'input':
-    #         data_names = self.inputs
-    #     if data_type == 'output':
-    #         data_names = self.outputs
-    #     for i in range(len(data_source)):
-    #         single_data_out = None
-    #         for data_name in data_names:
-    #             new_element = torch.tensor(data_source[i][data_name]).reshape((-1, 1))
-    #             if single_data_out is None:
-    #                 single_data_out = new_element
-    #             else:
-    #                 single_data_out = torch.cat((single_data_out, new_element), dim=0)
-    #         data_out.append(single_data_out)
-    #     return data_out
-
-    # def learn_base_model(self):
-    #     inputs = self.get_input_data("training")
-    #     outputs = self.get_output_data("training")
-    #     self.learn_grad(self.base_model, inputs, outputs)
-
-    # def learn_error_model(self):
-    #     inputs = self.get_input_data("training")
-    #     outputs = self.get_output_data("training")
-    #     temp_data_ = []
-    #     with torch.no_grad():
-    #         for i in range(len(inputs)):
-    #             out, _ = self.simulate_subsystem(self.base_model, inputs[i].reshape((-1, 1)))
-    #             temp_data_.append(out)
-    #
-    #     self.learn_grad(self.error_model, temp_data_, outputs)
-
-    # def apply_delay(self):
-    #     self.start_time = self.delay()
-
-    # def simulate_subsystem(self, subsystem, input_, output_=None):
-    #     loss = 0
-    #     TIME_LEN, INPUT_DIM = input_.shape
-    #     OUTPUT_DIM = subsystem.num_outputs
-    #     STATE_DIM = subsystem.num_states
-    #     states_predicted = torch.zeros((STATE_DIM,), dtype=torch.double)
-    #     out_predicted = torch.zeros((TIME_LEN, OUTPUT_DIM), dtype=torch.double)
-    #     for t in range(TIME_LEN):
-    #         out, states_predicted = subsystem(states_predicted, input_[t])
-    #         out_predicted[t] = out
-    #         if output_ is not None:
-    #             loss += self.loss_fn(out, output_[t])
-    #     return out_predicted, loss
-
-    # def learn_no_grad(self, subsystem):
-    #     pass
-
-    # def learn_grad(self, subsystem, inputs: list, outputs: list):
-    #     epochs = 100
-    #     NUM_SIGNALS = len(inputs)
-    #     optimizer = optim.Adam(subsystem.parameters(), lr=0.01)
-    #
-    #     print("Learning started")
-    #
-    #     for ep in range(epochs):
-    #         print("------")
-    #         for i in range(NUM_SIGNALS):
-    #             optimizer.zero_grad()
-    #             _, loss = self.simulate_subsystem(subsystem, inputs[i], outputs[i])
-    #             print(f"Loss: {loss}")
-    #             loss.backward()
-    #             optimizer.step()
 
     def randomize_samples(self):
         self.loaded_data = np.random.permutation(self.loaded_data)
