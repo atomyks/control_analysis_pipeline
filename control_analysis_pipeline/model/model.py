192fc9169929c583bbd935d38e8f7bb9eb0718cc
Ahmad Amine <ahmadamine998@gmail.com>
2023-08-22 18:13:14 +0900

    Merged base functional into model. Deadzone is now a model

--- control_analysis_pipeline/model/model.py
+++ control_analysis_pipeline/model/model.py
@@ -31,19 +31,11 @@ class Model(nn.Module):
         self.reg = RegressorFactory(batch_size=1, num_actions=self.num_actions,
                                     num_states=self.num_states, action_history_size=self.action_history_size,
                                     state_history_size=self.state_history_size)
-
-    def forward(self,
-                regressors: torch.tensor or None = None,
-                a_input: torch.tensor or None = None,
-                y_last: torch.tensor or None = None):
-        """
-        By default, the model simply returns the regressors. Override this function if you need to do some computation
-        :param regressors: torch.tensor, BATCH x NUM_REGRESSORS, model regressors
-        :param a_input: torch.tensor, BATCH x NUM_INPUTS, system action
-        :param y_last: torch.tensor, BATCH x NUM_STATES, system state
-        :return:
-        """
-        return regressors
+        
+        # Non-gradient parameters
+        self._nongrad_params = {}
+        self._nongrad_params_flat = {}
+        self._models = {}
 
     def get_regressors(self, u_input: torch.tensor, y_last: torch.tensor):
         """
@@ -63,14 +55,6 @@ class Model(nn.Module):
         """
 
         return len(self.reg)
-
-    def init_learning(self):
-        '''
-        Used to initialize the learning process. This function is called before the first call of the learn function.
-        Default implementation does nothing. Override this function if you need to do some initialization.
-        :return:
-        '''
-        pass
 
     def init_regressor_history(self, 
                                batch_size: int = 1,
@@ -94,6 +78,73 @@ class Model(nn.Module):
 
         self.reg.set_history(history_a, history_s)
         
+    def forward(self,
+                regressors: torch.tensor or None = None,
+                a_input: torch.tensor or None = None,
+                y_last: torch.tensor or None = None):
+        """
+        By default, the model simply returns the regressors. Override this function if you need to do some computation
+        :param regressors: torch.tensor, BATCH x NUM_REGRESSORS, model regressors
+        :param a_input: torch.tensor, BATCH x NUM_INPUTS, system action
+        :param y_last: torch.tensor, BATCH x NUM_STATES, system state
+        :return:
+        """
+        return regressors
+
+    def init_learning(self):
+        '''
+        Used to initialize the learning process. This function is called before the first call of the learn function.
+        Default implementation does nothing. Override this function if you need to do some initialization.
+        :return:
+        '''
+        pass
+
+    def register_nongrad_parameter(self, name, value):
+        '''
+        Registers a non-gradient parameter. This is optimized using a gradient-free optimizer.
+        :param name: Name of the parameter
+        :param value: Parameter to be added.
+        :return:
+        '''
+        self._nongrad_params[name] = value
+
+    def register_model(self, name, value):
+        '''
+        Registers a model. This allows for layered models.
+        :param name: Name of the model
+        :param value: Model to be added
+        :return:
+        '''
+        self._models[name] = value
+
+    def gen_nongrad_params_flat(self):
+        '''
+        Generates a flattened version of the non-gradient parameters by recursively flattening the submodels.
+        :return: Flattened dictionary of non-gradient parameters
+        '''
+
+        self._nongrad_params_flat = self._nongrad_params
+        for model_key in list(self._models.keys()):
+            submodel_nongrad_params = self._models[model_key].gen_nongrad_params_flat()
+            for submodel_key in list(submodel_nongrad_params.keys()):
+                flat_key = model_key + "." + submodel_key
+                self._nongrad_params_flat[flat_key] = submodel_nongrad_params[submodel_key]
+        return self._nongrad_params_flat
+
+    def get_search_space(self):
+        '''
+        Generates the search space for the model. This is used by the gradient-free optimizer to search for the optimal parameters.
+        :return: Search space and the parameters
+        '''
+
+        search_space = {}
+        optim_params = self.gen_nongrad_params_flat()
+        for name in list(optim_params.keys()):
+            search_space[name] = torch.arange(optim_params[name].lb,
+                                              optim_params[name].ub,
+                                              optim_params[name].precision)
+        return search_space, optim_params
+
     def __str__(self):
         '''
         String operator overload for printing the model to the console.
@@ -102,4 +153,13 @@ class Model(nn.Module):
         str_out = str(self.reg)
         str_out += "\n"
         str_out += super(Model, self).__str__()
+        str_out += "\n----------------------------------\n"
+        str_out += f"Nongrad params: {self._nongrad_params.keys()}\n"
+        str_out += f"Registered models: {self._models.keys()}\n"
         return str_out
+
+    def __repr__(self):
+        report = "\n----------------------------------\n"
+        report += f"Nongrad params: {self._nongrad_params.keys()}\n"
+        report += f"Registered models: {self._models.keys()}\n"
+        return report
