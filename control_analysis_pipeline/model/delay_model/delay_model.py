b76b3722aa6e02813a4e48898f59c555d2f1a5c3
Ahmad Amine <ahmadamine998@gmail.com>
2023-08-22 20:20:22 +0900

    Input delay model now inherits from model

--- control_analysis_pipeline/model/delay_model/delay_model.py
+++ control_analysis_pipeline/model/delay_model/delay_model.py
@@ -1,32 +1,28 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from control_analysis_pipeline.model.model import Model
+from control_analysis_pipeline.model.nongradient_parameter import NongradParameter
 import queue
 
-
-class DelayModel(nn.Module):
+class InputDelayModel(Model):
     """
-    System delay module. This cannot use backpropagation. This can be trained only using non-gradient optimization
+    System input delay module. This can be trained only using non-gradient optimization
     """
-    def __init__(self, batch_size=1, num_actions=1):
-        super(DelayModel, self).__init__()
+    def __init__(self, num_actions=1):
+        super(InputDelayModel, self).__init__(num_actions=num_actions)
+        self.delay_parm = NongradParameter("delay", 0, 10, 1)
+        self.register_nongrad_parameter("delay", self.delay_parm)
+        self.batch_size = 1
 
-        self.back_prop = False
-        self.nongrad_params = {
-            "delay": {
-                "lb": 0,
-                "ub": 10,
-                "val": 0,
-            }
-        }
-        self.input_queue = None
-
+    def init_learning(self, batch_size):
+        '''
+        Used to initialize the size of the queue. This function is called before the first call of the learn function. 
+        :return:
+        '''
         self.batch_size = batch_size
-        self.num_actions = num_actions
         self.reset()
 
     def forward(self, u_input: torch.tensor):
-        if self.nongrad_params["delay"]["val"] > 0:
+        if self.delay_parm.get() > 0:
             y_output = self.input_queue.get()
             self.input_queue.put(u_input.reshape((self.batch_size, self.num_actions)))
         else:
@@ -34,7 +30,7 @@ class DelayModel(nn.Module):
         return y_output
 
     def reset(self):
-        self.input_queue = queue.Queue(maxsize=self.nongrad_params["delay"]["val"])
-        for i in range(self.nongrad_params["delay"]["val"]):
+        self.input_queue = queue.Queue(maxsize=self.delay_parm.get())
+        for i in range(self.delay_parm.get()):
             u = torch.zeros((self.batch_size, self.num_actions), dtype=torch.float64)
             self.input_queue.put(u)
