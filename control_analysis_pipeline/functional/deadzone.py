305522cc7a73c768f754e85182bf1de523b38728
Tomas Nagy <nagytom4@fel.cvut.cz>
2023-08-26 01:16:09 +0900

    Add parameter setting for deadzone model

--- control_analysis_pipeline/functional/deadzone.py
+++ control_analysis_pipeline/functional/deadzone.py
@@ -3,18 +3,20 @@ import torch.nn as nn
 from control_analysis_pipeline.model.model import Model
 from control_analysis_pipeline.model.nongradient_parameter import NongradParameter
 
+
 class Deadzone(Model):
     """
     Deadzone model. Inherits from Model class, but only uses the non-gradient parameters.
     """
 
-    def __init__(self):
+    def __init__(self, r_lb=0.0, r_ub=20.0, r_precision=0.1,
+                 l_lb=-20.0, l_ub=0.0, l_precision=0.1):
         super(Deadzone, self).__init__()
 
-        self.r_lambd = NongradParameter(torch.zeros((1,)), lb=0., ub=20.1, precision=0.1)
+        self.r_lambd = NongradParameter(torch.zeros((1,)), lb=r_lb, ub=r_ub, precision=r_precision)
         self.register_nongrad_parameter(name="r_lambd", value=self.r_lambd)
 
-        self.l_lambd = NongradParameter(torch.zeros((1,)), lb=-20.1, ub=0., precision=0.01)
+        self.l_lambd = NongradParameter(torch.zeros((1,)), lb=l_lb, ub=l_ub, precision=l_precision)
         self.register_nongrad_parameter(name="l_lambd", value=self.l_lambd)
 
     def forward(self, x: torch.tensor):
@@ -23,6 +25,7 @@ class Deadzone(Model):
                   Parameters of deadzone are the same for each input element
         :return:
         """
-
         y = torch.where(x < self.l_lambd or self.r_lambd < x, x, 0.0)
+        # print(f"deadzone: x {x}  ->  y {y}  {x < self.l_lambd or self.r_lambd < x}, {self.l_lambd.get()}, ")
+
         return y
