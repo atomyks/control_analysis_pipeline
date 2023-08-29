e658ef4cfd20bbb9de86a77ac61ae8fa6d88b403
Tomas Nagy <nagytom4@fel.cvut.cz>
2023-08-30 02:03:33 +0900

    Add steering model to main pipeline

--- main.py
+++ main.py
@@ -32,17 +32,81 @@ def main():
 
     data = data_preprocessor.get_preprocessed_data()
 
-    sys = System(data, num_states=4)
+    sys = System(data, num_states=2, num_actions=1)
     sys.parse_config(config)
 
     sys.randomize_samples()
     sys.split_data()
 
-    inputs_arr = sys.get_data_from_dict('input', 'training')
-    outputs_arr = sys.get_data_from_dict('output', 'training')
+    input_train_arr = sys.get_data_from_dict('input', 'training')
+    input_test_arr = sys.get_data_from_dict('input', 'testing')
+    outputs_train_arr = sys.get_data_from_dict('output', 'training')
+
+    print("pipeline start")
+
+    print(len(input_train_arr))
+    print(len(input_test_arr))
+    print(input_train_arr[0][:-1, :].shape)
+    print(input_train_arr[1][:-1, :].shape)
+
+    print(len(outputs_train_arr))
+    print(outputs_train_arr[0].shape)
+    print(outputs_train_arr[1].shape)
+
+    from control_analysis_pipeline.model.base_model.base_model_steering_v2 import Steeringv2
+    from control_analysis_pipeline.model.base_model.base_model_simple_steering import SimpleSteering
+    import gradient_free_optimizers as gfo
+    import copy
+    # Learn system
+    # system = System(num_states=2, num_actions=1)
+    # sys.base_model = Steeringv2(dt=0.03)
+    sys.base_model = SimpleSteering(dt=0.03)
+
+    train_a = []
+    for i in range(len(input_train_arr)):
+        train_a.append(input_train_arr[i][:-1, :])
+
+    sys.nongrad_learn_base_model(
+        inputs=copy.deepcopy(train_a),  # [:-1, :]
+        true_outputs=copy.deepcopy(outputs_train_arr),
+        optimizer=gfo.ParticleSwarmOptimizer,
+        epochs=200,
+        verbose=True)
+
+    fig, axs = plt.subplots(2, 2)
+
+    # initial_state = torch.cat((outputs_train_arr[0][0:1, :], torch.zeros((1, 1))), dim=-1)
+    initial_state = outputs_train_arr[0][0:1, :]
+    sys.plot_simulation(input_array=copy.deepcopy(input_train_arr[0][:-1, :]),
+                        true_state=copy.deepcopy(outputs_train_arr[0][1:, :]),
+                        initial_state=initial_state,
+                        ax=axs[0][0], show_input=True, show_hidden_states=False,
+                        use_base_model=True, use_error_model=False)
+
+    # initial_state = torch.cat((outputs_train_arr[1][0:1, :], torch.zeros((1, 1))), dim=-1)
+    initial_state = outputs_train_arr[1][0:1, :]
+    sys.plot_simulation(input_array=copy.deepcopy(input_train_arr[1][:-1, :]),
+                        true_state=copy.deepcopy(outputs_train_arr[1][1:, :]),
+                        initial_state=initial_state,
+                        ax=axs[0][1], show_input=True, show_hidden_states=False,
+                        use_base_model=True, use_error_model=False)
+
+    # initial_state = torch.cat((outputs_train_arr[2][0:1, :], torch.zeros((1, 1))), dim=-1)
+    initial_state = outputs_train_arr[2][0:1, :]
+    sys.plot_simulation(input_array=copy.deepcopy(input_train_arr[2][:-1, :]),
+                        true_state=copy.deepcopy(outputs_train_arr[2][1:, :]),
+                        initial_state=initial_state,
+                        ax=axs[1][0], show_input=True, show_hidden_states=False,
+                        use_base_model=True, use_error_model=False)
+
+    handles, labels = axs[0][0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper right')
+    plt.show()
+
+    exit()
 
     # Train the model on the bags of data
-    sys.learn_grad(inputs=inputs_arr, true_outputs=outputs_arr, 
+    sys.learn_grad(inputs=input_train_arr, true_outputs=outputs_train_arr,
                    batch_size=2, optimizer=torch.optim.SGD, learning_rate=0.2,
                    epochs=100, use_delay=False, use_base_model=True, use_error_model=False)
 
@@ -65,59 +129,6 @@ def main():
     handles, labels = ax.get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper right')
     plt.show()
-
-    # u_init -> BATCH x history x inputs
-    # x_last -> BATCH x history x states
-
-    # u = torch.tensor([[[0.1], [0.1]], [[0.1], [0.1]]])
-    # x = torch.tensor([[[0.1], [0.1]], [[0.1], [0.1]]])
-    #
-    #
-    # # u = torch.tensor([[0.1], [0.1]])
-    # # x = torch.tensor([[0.1], [0.1]])
-    #
-    #
-    # print("********")
-    # print(u.shape)
-    # print(u.dim())
-    # print(x.shape)
-    #
-    # f = lambda u_input, x_last: torch.vstack([
-    #     u_input[..., 0, 0] + x_last[..., 0, 0],
-    #     u_input[..., 0, 0] + x_last[..., 0, 0] * 2.0,
-    #     u_input[..., 0, 0] + x_last[..., 0, 0] * 20.0,
-    # ])
-    #
-    # y = f(u_input=u, x_last=x)
-    #
-    # print(y.shape)
-    #
-    # print(y)
-
-    # sys.learn_base_model()
-    # sys.learn_error_model()
-
-    # from system.base_model_linear import BaseLinearModel
-    #
-    # base_model = BaseLinearModel()
-    #
-    # print(base_model)
-    # print(base_model.parameters())
-    #
-    # for param in base_model.parameters():
-    #     print(type(param), param.size())
-    #
-    # print("--------------------------------------------")
-    # from system.delay_model_sample import DelayModelSample
-    # print("Delay model")
-    # delay = DelayModelSample()
-    # print(delay)
-    #
-    # for param in delay.parameters():
-    #     print(type(param), param.size())
-    #
-    # print(delay.nongrad_params.keys())
-    # sys.learn_delay()
 
 
 if __name__ == "__main__":
