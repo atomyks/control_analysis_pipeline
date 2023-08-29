5765a382cd2e0a8cade22596aae21f9aaac28238
Tomas Nagy <nagytom4@fel.cvut.cz>
2023-08-30 01:55:51 +0900

    Bugfix and set parameters properly

--- control_analysis_pipeline/model/base_model/base_model_steering_v2.py
+++ control_analysis_pipeline/model/base_model/base_model_steering_v2.py
@@ -15,8 +15,8 @@ class Steeringv2(Model):
         self.delay_layer.reset()
         self.register_model("delay_layer", self.delay_layer)
 
-        self.deadzone_layer = Deadzone(r_lb=0.0011, r_ub=0.0013, r_precision=0.0001,
-                                       l_lb=-0.0012, l_ub=-0.0010, l_precision=0.0001)
+        self.deadzone_layer = Deadzone(r_lb=0.0, r_ub=0.01, r_precision=0.0001,
+                                       l_lb=-0.01, l_ub=-0.00, l_precision=0.0001)
         self.register_model("deadzone_layer", self.deadzone_layer)
 
         # self.steer_rate_e = NongradParameter(torch.zeros((1,)), lb=0.0, ub=1.0, precision=0.01)
@@ -30,25 +30,22 @@ class Steeringv2(Model):
         self.last_error = 0.0
         self.last_steer_rate = 0.0
 
-        self.Kp = NongradParameter(torch.zeros((1,)), lb=25.0, ub=26.0, precision=1.0)
+        self.Kp = NongradParameter(torch.zeros((1,)), lb=0.0, ub=100.0, precision=1.0)
         self.register_nongrad_parameter(name="Kp", value=self.Kp)
 
-        self.Ki = NongradParameter(torch.zeros((1,)), lb=0.012, ub=0.013, precision=0.001)
+        self.Ki = NongradParameter(torch.zeros((1,)), lb=0.0, ub=0.01, precision=0.001)
         self.register_nongrad_parameter(name="Ki", value=self.Ki)
 
-        self.Kd = NongradParameter(torch.zeros((1,)), lb=0.038, ub=0.039, precision=0.001)
+        self.Kd = NongradParameter(torch.zeros((1,)), lb=0.0, ub=0.05, precision=0.002)
         self.register_nongrad_parameter(name="Kd", value=self.Kd)
 
-        self.J = NongradParameter(torch.zeros((1,)), lb=0.7, ub=0.8, precision=0.1)
+        self.J = NongradParameter(torch.zeros((1,)), lb=0.0, ub=10, precision=0.1)
         self.register_nongrad_parameter(name="J", value=self.J)
 
-        self.b3 = NongradParameter(torch.zeros((1,)), lb=8.2, ub=8.3, precision=0.1)
+        self.b3 = NongradParameter(torch.zeros((1,)), lb=0, ub=10, precision=0.1)
         self.register_nongrad_parameter(name="b3", value=self.b3)
 
-        self.T_f = NongradParameter(torch.zeros((1,)), lb=0.0, ub=0.001, precision=0.001)
-        self.register_nongrad_parameter(name="T_f", value=self.T_f)
-
-        self.max_vel = NongradParameter(torch.zeros((1,)), lb=99.0, ub=100.0, precision=1.0)
+        self.max_vel = NongradParameter(torch.zeros((1,)), lb=0.0, ub=1000.0, precision=10.0)
         self.register_nongrad_parameter(name="max_vel", value=self.max_vel)
 
         # self.max_vel = None
@@ -73,8 +70,7 @@ class Steeringv2(Model):
         self.last_error = error
 
         # Virtual system
-        y_output = state + self.system_get_f(state, self.J.get(), T_in, self.b3.get(),
-                                              self.T_f.get()) * self.dt  # Forward euler
+        y_output = state + self.system_get_f(state, self.J.get(), T_in, self.b3.get()) * self.dt  # Forward euler
 
         y_output[:, 1] = self.deadzone_layer(y_output[:, 1])
         # y_output[:, 1] = min(max(y_output[:, 1], -self.max_vel.get()), self.max_vel)
@@ -87,12 +83,12 @@ class Steeringv2(Model):
 
         return y_output, delayed_input
 
-    def system_get_f(self, x, J, T_in, b3, T_f):
+    def system_get_f(self, x, J, T_in, b3):
         # x (BATCH x 2);  x[:, 0] = angle; x[:, 1] - steering speed
         # x1 = x[:, 0]
         x2 = x[:, 1]
         x1_dot = x2
-        x2_dot = 1.0 / J * (T_in - b3 * x2 - T_f)
+        x2_dot = 1.0 / J * (T_in - b3 * x2)
 
         x_dot = torch.cat((x1_dot.reshape((self.batch_size, 1)), x2_dot.reshape((self.batch_size, 1))), dim=-1)
         return x_dot
