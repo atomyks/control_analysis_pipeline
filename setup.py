6d4a68253f2de02fef1fbcddeedfed641dcbde84
Tomas Nagy <nagytom4@fel.cvut.cz>
2023-11-28 16:23:38 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ctrltool',
+      version='0.0.1',
+      author='Tomas Nagy, Ahmad Amine',
+      author_email='nagytom@tier4.jp',
+      url='',
+      package_dir={'': 'src'},
+      install_requires=['numpy']  # TODO add all of the requirements
+      )
