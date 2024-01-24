0f27c19ca92043682020349b9c1f08e60f57e6c0
Tomas Nagy <nagytom4@fel.cvut.cz>
2024-01-24 20:16:42 +0100

    Update autoware models

--- src/control_analysis_pipeline/model/base_model/kinematic_bicycle_steer_vel.py
+++ src/control_analysis_pipeline/model/base_model/kinematic_bicycle_steer_vel.py
@@ -8,16 +8,16 @@ from control_analysis_pipeline.model.delay_model.delay_model import InputDelayMo
 
 class KinematicBicycleSteerVel(Model):
     def __init__(self, dt: float = 0.1):
-        super(KinematicBicycleSteerVel, self).__init__(num_actions=2, num_states=3, 
+        super(KinematicBicycleSteerVel, self).__init__(num_actions=2, num_states=4, 
                                                  sig_action_names=["STEER", "VX"], 
-                                                 sig_state_names=["POS_X", "POS_Y", "YAW"])
+                                                 sig_state_names=["POS_X", "POS_Y", "YAW", "YAW_RATE"])
         self.batch_size = 1
 
         # Register additional model parameters
 
         self.wheelbase = NongradParameter(torch.zeros((1,)), trainable=False)
         self.register_nongrad_parameter(name="wheelbase", value=self.wheelbase)
-        self.wheelbase.set(2.2)
+        self.wheelbase.set(2.74)
 
         self.dt = NongradParameter(torch.zeros((1,)), trainable=False)
         self.register_nongrad_parameter(name="dt", value=self.dt)
@@ -46,9 +46,11 @@ class KinematicBicycleSteerVel(Model):
         f_x[:, 0] = v_x * torch.cos(yaw)
         f_x[:, 1] = v_x * torch.sin(yaw)
         f_x[:, 2] = v_x * torch.tan(steer) / self.wheelbase.get()
+        f_x[:, 3] = 0
 
         # Integrate the model
         next_state = state + f_x * self.dt.get() # Forward euler
+        next_state[:, 3] = v_x * torch.tan(steer) / self.wheelbase.get()
 
         return next_state, action
 
